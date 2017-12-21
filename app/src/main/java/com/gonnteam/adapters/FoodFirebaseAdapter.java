5622dadcd2c5a0d64ce3cb3dc69232387722fc0d
Huynh Thien <thienromtn55@gmail.com>
2017-12-21 22:32:52 +0700

    temp

--- app/src/main/java/com/gonnteam/adapters/FoodFirebaseAdapter.java
+++ app/src/main/java/com/gonnteam/adapters/FoodFirebaseAdapter.java
@@ -3,11 +3,13 @@ package com.gonnteam.adapters;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,9 +18,16 @@ import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.gonnteam.R;
 import com.gonnteam.activities.FoodDetailActivity;
+import com.gonnteam.activities.LoginActivity;
 import com.gonnteam.models.Food;
 import com.gonnteam.models.Like;
-import com.google.firebase.database.DataSnapshot;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -39,6 +48,12 @@ public class FoodFirebaseAdapter {
     private Context context;
     private Query query;
     private Like like;
+    private boolean mProcessLike = false;
+    private CollectionReference mLikeRef;
+    private CollectionReference mFoodRef;
+    private String foodID;
+    private FirebaseUser fuser;
+    private FirebaseAuth mAuth;
     public FoodFirebaseAdapter(final Query query, final Context context) {
         this.context = context;
         this.query = query;
@@ -66,7 +81,7 @@ public class FoodFirebaseAdapter {
             @Override
             protected void onBindViewHolder(FoodViewHolder holder, final int position, Food model) {
 
-                final String ID = getSnapshots().getSnapshot(position).getId();
+                foodID = getSnapshots().getSnapshot(position).getId();
                 final Food food = data.get(position);
                 holder.setTitle(food.getTitle());
                 holder.setCmt(food.getComment());
@@ -80,11 +95,40 @@ public class FoodFirebaseAdapter {
                         Bundle bundle = new Bundle();
                         bundle.putSerializable("food", food);
                         detail.putExtra("bundle",bundle);
-                        detail.putExtra("foodID", ID);
+                        detail.putExtra("foodID", foodID);
                         context.startActivity(detail);
                     }
                 });
+                holder.btnLike.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //check user already login
+                        mAuth = FirebaseAuth.getInstance();
+                        fuser = mAuth.getCurrentUser();
+                        if (fuser == null){
+                            Intent login = new Intent(context, LoginActivity.class);
+                            context.startActivity(login);
+                        } else {
+                            String userID = fuser.getUid();
+                            mProcessLike = true;
+                            mLikeRef = FirebaseFirestore.getInstance().collection("likes/" + foodID);
+                            mLikeRef.add(userID)
+                                    .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<DocumentReference> task) {
 
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+
+                                        }
+                                    });
+                        }
+
+                    }
+                });
 
             }
 
@@ -98,9 +142,10 @@ public class FoodFirebaseAdapter {
     public static class FoodViewHolder extends RecyclerView.ViewHolder {
 
         private ImageView imgBackdrop;
-
+        private ImageButton btnLike;
         public FoodViewHolder(View itemView) {
             super(itemView);
+            btnLike = itemView.findViewById(R.id.btnLike);
         }
 
         public void setTitle(String title){

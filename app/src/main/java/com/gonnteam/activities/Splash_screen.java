833e6f631bf17ec32769039ec973464c3592aea0
Mphuong144 <goodboyvt1996@gmail.com>
2018-06-18 00:54:56 +0700

    splash screen

--- app/src/main/java/com/gonnteam/activities/Splash_screen.java
+++ app/src/main/java/com/gonnteam/activities/Splash_screen.java
@@ -5,19 +5,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.gonnteam.R;
 
 public class Splash_screen extends AppCompatActivity {
-    public TextView tv;
+    public ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
-        tv = (TextView) findViewById(R.id.textView2);
+        iv = (ImageView) findViewById(R.id.imageView2);
         Animation anim = AnimationUtils.loadAnimation(this, R.anim.transition);
-        tv.startAnimation(anim);
+        iv.startAnimation(anim);
         final Intent i = new Intent(this, MainActivity.class);
         Thread timer  = new Thread(){
             public void run (){
